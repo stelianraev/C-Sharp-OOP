d9a84eb94208e879f8b3b094e62dfe0c4bc62029
Stelian Raev <40591640+stelianraev@users.noreply.github.com>
2020-06-12 15:20:38 +0300

    Add files via upload

---
+++ Reflection and Attributes/01. CommandPattern/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using CommandPattern.Core;
+using CommandPattern.Core.Contracts;
+using System;
+using System.Reflection;
+
+namespace CommandPattern
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            ICommandInterpreter command = new CommandInterpreter();
+            IEngine engine = new Engine(command);
+            engine.Run();
+
+
+        }
+    }
+}
---
+++ Reflection and Attributes/02. ValidationAttributes/Attributes/MyRequiredAttrebute.cs
@@ -0,0 +1,14 @@
+﻿namespace ValidationAttributes.Attributes
+{
+    public class MyRequiredAttrebute : MyValidationAttribute
+    {
+        public override bool IsValid(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
